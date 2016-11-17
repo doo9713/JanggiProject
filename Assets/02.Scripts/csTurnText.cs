f9f8f3d80f3d8623334e65a5984378576bb11710
doo9713 <doo9713@gmail.com>
2016-11-17 20:35:56 +0900

    ..

---
+++ Assets/02.Scripts/csTurnText.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class csTurnText : MonoBehaviour
+{
+    Text text;
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (csMain.player)
+            text.text = "Player1";
+        else
+            text.text = "Player2";
+    }
+}
